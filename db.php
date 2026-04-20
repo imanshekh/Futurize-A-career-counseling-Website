e85fc0aace7bbf72eddb63a7aa3c02cb87245428
iman shekh <imansheikh09@gmail.com>
2026-04-20 14:35:24 +0530

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "futurize_db"; // your database name
+$dbname = "futurize_db"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
